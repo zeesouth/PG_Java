02ff6ae16bab09805aa1bd94efbc32eb181bd4cc
Jisu <jisunam0307@gmail.com>
2023-05-05 22:01:13 +0900

    최소직사각형 (LV1, 86491)

--- src/lv1/n86491/Solution.java
+++ src/lv1/n86491/Solution.java
@@ -1,7 +1,26 @@
 package lv1.n86491;
 
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println("HELLO");
+class Solution {
+    public int solution(int[][] sizes) {
+
+        int wMax = Integer.MIN_VALUE;
+        int hMax = Integer.MIN_VALUE;
+
+        for(int i=0;i<sizes.length;i++) {
+
+            int w = sizes[i][0], h = sizes[i][1];
+
+            if(w < h) {
+                int tmp = w;
+                w = h;
+                h = tmp;
+            }
+
+            wMax = Math.max(w, wMax);
+            hMax = Math.max(h, hMax);
+
+        }
+
+        return wMax*hMax;
     }
 }
