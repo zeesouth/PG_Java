35a7b5a7f916ae911ca03f2e939d4db6fd6ed052
Jisu <jisunam0307@gmail.com>
2023-05-05 22:03:25 +0900

    H-Index (LV2, 42747)

---
+++ src/lv2/n42747/Solution.java
@@ -0,0 +1,42 @@
+package lv2.n42747;
+
+// 1 <= n <= 1000
+// 0 <= h <= 10000
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int indexMin = Integer.MAX_VALUE;
+        int indexMax = Integer.MIN_VALUE;
+
+
+        for(int i=0;i<citations.length;i++) {
+            indexMin = Math.min(citations[i], indexMin);
+            indexMax = Math.max(citations[i], indexMax);
+        }
+
+        for(int i=indexMax;i>=indexMin;i--) {
+            int count = 0;
+            for(int j=0;j<citations.length;j++){
+                if(citations[j] >= i) count++;
+            }
+
+            if(count > i) {
+                return i;
+            }
+
+            if(count < i && i == indexMin) {
+                return count;
+            }
+
+            if(count == i) {
+                answer = count;
+                break;
+            }
+        }
+
+
+
+        return answer;
+    }
+}
