0b5bc1bda4c29a3eea9102b71299e7ee76e29769
Jisu <jisunam0307@gmail.com>
2023-05-05 22:56:36 +0900

    게임 맵 최단거리 (LV2, 1844)

---
+++ src/lv2/n1844/Solution.java
@@ -0,0 +1,62 @@
+package lv2.n1844;
+
+import java.util.*;
+
+class Solution {
+
+    static int answer;
+    static int xLen, yLen;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static boolean[][] visited;
+
+    public int solution(int[][] maps) {
+        xLen = maps.length;
+        yLen = maps[0].length;
+        visited = new boolean[xLen][yLen];
+
+        answer = bfs(maps);
+
+        return answer;
+    }
+
+    public static boolean isValid(int x, int y) {
+        return x >= 0 && x < xLen && y >= 0 && y < yLen;
+    }
+
+    public int bfs(int[][] maps) {
+
+        Queue<int[]> q = new LinkedList<>();
+        // start
+        q.add(new int[]{0, 0, 1});
+        visited[0][0] = true;
+        int count = Integer.MAX_VALUE;
+        boolean flag = false;
+
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+
+            if(curr[0] == xLen-1 && curr[1] == yLen-1) {
+                count = Math.min(count, curr[2]);
+                flag = true;
+            }
+
+            for(int i=0;i<4;i++) {
+                int currX = curr[0]+dx[i];
+                int currY = curr[1]+dy[i];
+
+                if(isValid(currX, currY) && maps[currX][currY] != 0) {
+                    if(!visited[currX][currY]) {
+                        visited[currX][currY] = true;
+                        q.add(new int[]{currX, currY, curr[2]+1});
+                    }
+                }
+            }
+        }
+
+        return flag ? count : -1;
+
+    }
+
+}
+
