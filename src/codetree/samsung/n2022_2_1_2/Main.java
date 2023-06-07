d80d93d87aa21a2565dc7bd66edb1a84d58a7db0
Jisu <jisunam0307@gmail.com>
2023-06-07 20:43:04 +0900

    산타의 선물 공장 (코드트리, 2022 삼성 하반기 오전 2번) [simulation, DL]

--- src/codetree/samsung/n2022_2_1_2/Main.java
+++ src/codetree/samsung/n2022_2_1_2/Main.java
@@ -5,12 +5,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static final int MAX_M = 10;
+
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int N, M, size;
-    static Map<Integer, Belt> map;
-    static ArrayList<Belt> arr;
-    // static Belt[] belt;
+
+    static int N, M;
+
+    // id별로 상자 무게 관리
+    static Map<Integer, Integer> weight = new HashMap<>();
+
+    // id별로 상자의 next, prev 관리, 0일경우 없다는 뜻
+    static Map<Integer, Integer> prev = new HashMap<>();
+    static Map<Integer, Integer> next = new HashMap<>();
+
+    // 벨트별로 head, tail, id 관리
+    // 0이면 없다는 뜻
+    static int head[] = new int[MAX_M];
+    static int tail[] = new int[MAX_M];
+
+    // 벨트가 망가졌는지 확인
+    static boolean broken[] = new boolean[MAX_M];
+
+    // 물건 별로 벨트 번호 기입
+    // 벨트 번호가 0이면 사라진 물건
+    static Map<Integer, Integer> beltNum = new HashMap<>();
+
     public static void main(String[] args) throws Exception {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,10 +39,10 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int order = Integer.parseInt(st.nextToken());
             if(order == 100) init();
-            else if(order == 200) sb.append(getOutPresents()).append("\n");
-            else if(order == 300) sb.append(checkPresents(true)).append("\n");
-            else if(order == 400) sb.append(checkPresents(false)).append("\n");
-            else sb.append(isBrokenBelt()).append("\n");
+            else if(order == 200) sb.append(getOutPresent()).append("\n");
+            else if(order == 300) sb.append(removePresent()).append("\n");
+            else if(order == 400) sb.append(checkPresent()).append("\n");
+            else sb.append(brokeBelt()).append("\n");
         }
         System.out.println(sb);
         br.close();
@@ -32,178 +52,197 @@ public class Main {
     static void init() {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        size = N/M;
-        map = new HashMap<>();
-        arr = new ArrayList<>();
-        for(int i=0;i<M;i++) {
-            Belt newBelt = new Belt(i);
-            map.put(i, newBelt);
-            arr.add(newBelt);
-        }
 
-        int ids[] = new int[N];
+        int[] ids = new int[N];
+        int[] ws = new int[N];
+
         for(int i=0;i<N;i++) ids[i] = Integer.parseInt(st.nextToken());
+        for(int i=0;i<N;i++) ws[i] = Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<N;i++) {
-            int pId = ids[i];
-            int weight = Integer.parseInt(st.nextToken());
-            int bId = i/size;
-            arr.get(bId).addPresent(new Present(pId, weight));
-        }
+        for(int i=0;i<N;i++) weight.put(ids[i], ws[i]);
 
-    }
+        int size = N/M;
+        for(int i=0;i<M;i++) {
+            // 각 벨트별로 head, tail 설정
+            head[i] = ids[i*size];
+            tail[i] = ids[(i+1)*size-1];
 
-    static class Belt {
-        Present head, tail;
-        int id, cnt;
+            for(int j = i*size; j<(i+1)*size;j++) {
+                // 상자 ID마다 벨트 번호 기입하기
+                beltNum.put(ids[j], i+1);
 
-        Belt(int id) {
-            this.id = id;
-            this.head = this.tail = null;
-            this.cnt = 0;
-        }
-
-        void addPresent(Present newP) {
-            if(this.head == null) {
-                this.head = this.tail = newP;
-                this.head.back = this.tail;
-                this.tail.front = this.head;
-            } else {
-                newP.front = this.tail;
-                this.tail.back = newP;
-                this.tail = newP;
-            }
-            this.cnt++;
-        }
-
-        // 2. 물건 하차
-        int getOutPresent(int weight) {
-            if(this.cnt == 0) return 0;
-
-            if(this.head.weight > weight) {
-                this.tail.back = this.head;
-                this.head.front = this.tail;
-                this.tail = this.head;
-                this.head = this.head.back;
-                this.tail.back = null;
-                return 0;
-            }
-
-            int res = head.weight;
-            if(this.cnt == 1) this.head = this.tail = null;
-            else {
-                this.head = this.head.back;
-                this.head.front = null;
-            }
-            this.cnt--;
-            return res;
-        }
-
-        // 3. 물건 제거
-        void removePresent(Present p) {
-            if(this.cnt == 1) this.head = this.tail = null;
-            else {
-                if(p == this.head) {
-                    this.head = this.head.back;
-                    this.head.front = null;
-                } else if(p == this.tail) {
-                    this.tail = this.tail.front;
-                    this.tail.back = null;
-                } else {
-                    Present f = p.front;
-                    Present b = p.back;
-                    f.back = b;
-                    b.front = f;
+                // next, prev 설정하기
+                if(j < (i+1) * size - 1) {
+                    next.put(ids[j], ids[j+1]);
+                    prev.put(ids[j+1], ids[j]);
                 }
             }
-            this.cnt--;
+        }
+    }
+
+    // id에 해당하는 상자 삭제하기
+    static void removedId(int id, boolean removeBelt) {
+        int bNum = beltNum.get(id)-1;
+
+        // 벨트 제거 옵션이 true라면 벨트 제거
+        if(removeBelt) beltNum.put(id, 0);
+
+        // 벨트의 원소가 하나라면
+        if(head[bNum] == tail[bNum]) head[bNum] = tail[bNum] = 0;
+
+            // 삭제 되는게 head라면 head만 변경
+        else if(id == head[bNum]) {
+            int nextId = next.get(id);
+            head[bNum] = nextId;
+            prev.put(nextId, 0);
         }
 
-        // 4. 물건 확인
-        void moveFirstPresent(Present p) {
-            this.head.front = this.tail;
-            this.tail.back = this.head;
-            this.tail = p.front;
-            this.tail.back = null;
-            this.head = p;
-            this.head.front = null;
+        // 삭제 되는게 tail이라면 tail만 변경
+        else if(id == tail[bNum]) {
+            int prevId = prev.get(id);
+            tail[bNum] = prevId;
+            next.put(prevId, 0);
         }
 
-        // 3, 4 물건 id 찾기
-        Present checkPresent(int pId) {
-            Present curr = this.head;
-            while(curr != null) {
-                if(curr.id == pId) break;
-                curr = curr.back;
-            }
-            return curr;
+        // 삭제 되는게 중간에 있는 것이라면 next, prev 모두 변경
+        else {
+            int prevId = prev.get(id), nextId = next.get(id);
+            next.put(prevId, nextId);
+            prev.put(nextId, prevId);
         }
 
-        // 5. 고장난 벨트의 물건 잇기
-        void connect(Present newF, Present newB, int cnt) {
-            this.tail.back = newF;
-            newF.front = this.tail;
-            this.tail = newB;
-            this.cnt += cnt;
-        }
+        // next, prev 지우기
+        next.put(id, 0);
+        prev.put(id, 0);
+    }
 
-        public boolean equals(Belt o) {
-            return this.id == o.id;
-        }
+    // targetId 바로 뒤에 id 추가
+    static void pushId(int targetId, int id) {
+        next.put(targetId, id);
+        prev.put(id, targetId);
+
+        // targetId가 tail이었다면 id로 tail변경
+        int bNum = beltNum.get(targetId)-1;
+        if(tail[bNum] == targetId) tail[bNum] = id;
     }
 
     // 200. 물건 하차
-    static int getOutPresents() {
-        int weight = Integer.parseInt(st.nextToken());
+    static int getOutPresent() {
+        int mWeight = Integer.parseInt(st.nextToken());
+
         int res = 0;
-        for(int i=0;i<arr.size();i++) {
-            res += arr.get(i).getOutPresent(weight);
+        for(int i=0;i<M;i++) {
+            // 망가진 벨트라면 넘어가기
+            if(broken[i]) continue;
+
+            // 벨트의 head 확인
+            if(head[i] != 0) {
+                int id = head[i];
+                int w = weight.get(id);
+
+                // 가장 앞에 있는 상자의 무게가 mWeight 이하라면 하차
+                if(w <= mWeight) {
+                    res += w;
+
+                    removedId(id, true);
+                } else if(next.get(id) != 0) {
+                    // 그렇지 않다면 상자를 맨 뒤로 올리기
+
+                    // 제거한 후
+                    removedId(id, false);
+
+                    // 맨 뒤에 push
+                    pushId(tail[i], id);
+                }
+            }
         }
+
         return res;
     }
 
     // 300. 물건 제거
-    // 400. 물건 확인
-    static int checkPresents(boolean isRemove) {
-        int pId = Integer.parseInt(st.nextToken());
-        for(int i=0;i<arr.size();i++) {
-            Present p = arr.get(i).checkPresent(pId);
-            if(p != null) {
-                if(isRemove) {
-                    arr.get(i).removePresent(p);
-                    return pId;
-                } else {
-                    arr.get(i).moveFirstPresent(p);
-                    return arr.get(i).id+1;
-                }
-            }
+    static int removePresent() {
+        int rId = Integer.parseInt(st.nextToken());
+
+        // 제거되지 않은 상자라면
+        if(beltNum.getOrDefault(rId, 0) != 0) {
+            removedId(rId, true);
+            return rId;
         }
+
+        // 이미 제거된 상자라면 -1
         return -1;
     }
 
 
-    // 5. 벨트 고장 여부
-    static int isBrokenBelt() {
-        int b_num = Integer.parseInt(st.nextToken())-1;
-        if(!map.containsKey(b_num)) return -1;
-        Belt target = map.get(b_num);
-        int rmId = 0;
-        for(int i=0;i<arr.size();i++) {
-            if(arr.get(i).id == b_num) continue;
-            arr.get(i).connect(target.head, target.tail, target.cnt);
-            break;
+    // 400. 물건 확인
+    static int checkPresent() {
+        int fId = Integer.parseInt(st.nextToken());
+        if(beltNum.getOrDefault(fId, 0) == 0) return -1;
+
+        // 상자를 찾아서 맨 앞으로 당겨준다.
+        // 찾은 상자가 head가 아닐 경우에만 유효
+        int bNum = beltNum.get(fId)-1;
+        if(head[bNum] != fId) {
+            int originTail = tail[bNum];
+            int originHead = head[bNum];
+
+            // 새로운 tail 갱신
+            int newTail = prev.get(fId);
+            tail[bNum] = newTail;
+            next.put(newTail, 0);
+
+            // 기존 tail의 next를 head로
+            // head의 prev를 기존 tail로 만들기
+            next.put(originTail, originHead);
+            prev.put(originHead, originTail);
+
+            // 새로운 head 지정하기
+            head[bNum] = fId;
         }
-        map.remove(b_num);
-        arr.remove(target);
-        return b_num+1;
+
+        return bNum+1;
     }
 
-    static class Present {
-        int id, weight;
-        Present front, back;
-        Present(int id, int weight) {
-            this.id = id;
-            this.weight = weight;
+
+    // 5. 벨트 고장시키기
+    static int brokeBelt() {
+        int bNum = Integer.parseInt(st.nextToken())-1;
+
+        if(broken[bNum]) return -1;
+
+        broken[bNum] = true;
+
+        if(head[bNum] == 0) return bNum+1;
+
+        // 해당 벨트의 오른쪽으로 순회하며 망가지지 않은 벨트 위로 상자를 옮기기
+        int nextNum = bNum;
+        while(true) {
+            nextNum = (nextNum+1)%M;
+
+            if(!broken[nextNum]) {
+                // 벨트가 비어있다면 그대로 옮겨주기
+                if(tail[nextNum] == 0) {
+                    head[nextNum] = head[bNum];
+                    tail[nextNum] = tail[bNum];
+                } else {
+                    // 선택한 벨트 뒤로 모두 옮겨주기
+                    pushId(tail[nextNum], head[bNum]);
+                    tail[nextNum] = tail[bNum];
+                }
+
+                // head~tail까지 순회하며 beltNum 갱신
+                int id = head[bNum];
+                while(id != 0) {
+                    beltNum.put(id, nextNum + 1);
+                    id = next.getOrDefault(id, 0);
+                }
+
+                head[bNum] = tail[bNum] = 0;
+                break;
+            }
         }
+        return bNum+1;
     }
+
 }
