71da64ce3bda04d8298864b74ff3e0fb5fd9aeeb
Jisu <jisunam0307@gmail.com>
2024-06-27 18:53:39 +0900

    양궁대회 (프로그래머스, Lv2) [DFS, Bactracking]

---
+++ src/programmers/lv2/n92342/Solution.java
@@ -0,0 +1,56 @@
+package programmers.lv2.n92342;
+
+
+import java.util.*;
+
+class Solution {
+    int a = 0;
+    int max = -1;
+    int[] answer = {-1};
+    int[] arr;
+
+    public int[] solution(int n, int[] info) {
+        for (int i = 0; i < info.length; i++) {
+            if (info[i] > 0) a += (10 - i);
+        }
+        arr = info;
+
+        dfs(10, 0, n, new int[11]);
+
+        return answer;
+    }
+
+    void dfs(int idx, int sum, int cnt, int[] list) {
+        if (cnt == 0 || idx == -1) {
+            if (a >= sum) return;
+
+            if (sum - a > max) {
+                answer = Arrays.copyOf(list, 11);
+                if (cnt > 0) answer[11] += cnt;
+                max = sum - a;
+            }
+
+            return;
+        }
+
+        for (int i = cnt; i >= 0; i--) {
+            if (cnt - i < 0) continue;
+
+            list[idx] = i;
+            boolean flag = arr[idx] < list[idx] && arr[idx] != 0;
+            if (flag) {
+                a -= 10 - idx;
+            }
+
+            dfs(idx - 1, sum + (arr[idx] < list[idx] ? 10 - idx : 0), cnt - i, list);
+
+            if (flag) {
+                a += 10 - idx;
+            }
+            list[idx] = 0;
+        }
+
+
+    }
+}
+}
